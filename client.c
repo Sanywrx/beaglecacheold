b7d9c05b8181c1c5893aad966af4b50303c2168b
dalequark <damarkowitz@gmail.com>
2013-11-25 16:10:59 +0000

    Got logging working on client (with one child)

--- client.c
+++ client.c
@@ -1,49 +1,61 @@
-/***************************************************************************
- *                                  _   _ ____  _
- *  Project                     ___| | | |  _ \| |
- *                             / __| | | | |_) | |
- *                            | (__| |_| |  _ <| |___
- *                             \___|\___/|_| \_\_____|
- *
- * Copyright (C) 1998 - 2013, Daniel Stenberg, <daniel@haxx.se>, et al.
- *
- * This software is licensed as described in the file COPYING, which
- * you should have received as part of this distribution. The terms
- * are also available at http://curl.haxx.se/docs/copyright.html.
- *
- * You may opt to use, copy, modify, merge, publish, distribute and/or sell
- * copies of the Software, and permit persons to whom the Software is
- * furnished to do so, under the terms of the COPYING file.
- *
- * This software is distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY
- * KIND, either express or implied.
- *
- ***************************************************************************/ 
 #include <stdio.h>
 #include <curl/curl.h>
- 
-int main(void)
+#include <errno.h>
+
+int main(int argp, char** argv)
 {
   CURL *curl = NULL;
   CURLcode res;
   CURLINFO info;
+
+  FILE* logFile = NULL;
+
   double totTime = -1;
- 
+  int i;
+  int numRequests;
+
+  int requestNumber = 0;
+
+  if(argp != 4)
+  {
+    printf("Usage: prog numRequests numProcesses logFile");
+    return;
+  }
+
+  numRequests = atoi(argv[1]);
+
+  /* Open the log file */
+  if( !(logFile = fopen(argv[3], "a")) )
+  {
+    printf("Could not open logfile:\n");
+    strerror(errno);
+    return;
+  }
+
   curl = curl_easy_init();
 
   if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "http://140.211.167.51");
-  
- 
-    /* Perform the request, res will get the return code */ 
-    res = curl_easy_perform(curl);
-    res = curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME, &totTime);
-    fprintf(stdout, "%f", totTime);
-    /* Check for errors */ 
-    if(res != CURLE_OK)
-      fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              curl_easy_strerror(res));
- 
+    for(i = 0; i < numRequests; i++)
+    {
+      curl_easy_setopt(curl, CURLOPT_URL, "http://140.211.167.51");
+      /* Perform the request, res will get the return code */ 
+      res = curl_easy_perform(curl);
+      requestNumber++;
+
+      /* Check for errors */ 
+      if(res != CURLE_OK)
+      {
+        fprintf(stderr, "curl_easy_perform() failed: %s\n",
+        curl_easy_strerror(res));
+      }
+      else
+      {
+          curl_easy_getinfo(curl, CURLINFO_TOTAL_TIME, &totTime);
+          fprintf(logFile, "client pid: %d Request number: %d Time taken: %f\n", 
+            (int) getpid(), requestNumber, totTime);
+      }
+    
+    }
     /* always cleanup */ 
     curl_easy_cleanup(curl);
   }
